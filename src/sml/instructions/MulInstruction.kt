36cbb3caa6f35ae142a1763a08681164917ecda6
gvince01 <gvince01@dcs.bbk.ac.uk>
2018-01-26 01:34:57 +0000

    made mulInstruction subclass

---
+++ src/sml/instructions/MulInstruction.kt
@@ -0,0 +1,24 @@
+package sml.instructions
+
+
+import sml.Instruction
+import sml.Machine
+
+/**
+ *@author gvince01
+ *
+ */
+
+
+class SubInstruction(label: String, val result: Int, val op1: Int, val op2: Int) : Instruction(label, "mul"){
+
+    override fun execute(m: Machine) {
+        val value1 = m.registers.getRegister(op1)
+        val value2 = m.registers.getRegister(op2)
+        m.registers.setRegister(result, value1 * value2)
+    }
+
+    override fun toString(): String{
+        return super.toString() + " " + op2 + " * " + op1 + " to " + result
+    }
+}
