481b876b014fdba6cc9be4e246e818a377077c7c
George Vincent <gvince01@dcs.bbk.ac.uk>
2018-02-07 17:03:56 +0000

    trying to get outInstruction to print the value of the register -- currently doesn't work

--- src/sml/instructions/OutInstruction.kt
+++ src/sml/instructions/OutInstruction.kt
@@ -2,6 +2,7 @@ package sml.instructions
 
 import sml.Instruction
 import sml.Machine
+import kotlin.coroutines.experimental.buildIterator
 
 /**
  *@author gvince01
@@ -11,10 +12,10 @@ import sml.Machine
 class OutInstruction(label: String, val op1 : Int) : Instruction(label, "out"){
 
     override fun execute(m: Machine) {
-        var outString = m.registers.getRegister(op1)
+        val outString = m.registers.getRegister(op1)
     }
 
     override fun toString(): String {
-        return super.toString() + " the value of " + op1 + " is "
+        return super.toString() + " the value of register " + op1 + " is need to get working"
     }
 }
